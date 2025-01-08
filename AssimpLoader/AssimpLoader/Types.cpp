a6ccd1e71e53ced77d9cf818eb731cb581d99f5b
ULOL <jeffk0408@naver.com>
2025-01-08 14:08:01 +0900

    refactor : move method to cpp from h

---
+++ AssimpLoader/AssimpLoader/Types.cpp
@@ -0,0 +1,35 @@
+﻿#include "Pch.h"
+#include "Types.h"
+
+void MaterialData::SetTextureFilesFromAiMaterialByTextureType( const aiMaterial * const Material, aiTextureType InTextureType )
+{
+	vector<string> * TargetList = nullptr;
+	switch (InTextureType)
+	{
+	case aiTextureType_DIFFUSE:  // NOLINT(bugprone-branch-clone)
+		TargetList = &DiffuseFiles;
+		break;
+	case aiTextureType_SPECULAR:
+		TargetList = &SpecularFiles;
+		break;
+	case aiTextureType_NORMALS:
+		TargetList = &NormalFiles;
+		break;
+	default:
+		break;
+	}
+	if (TargetList == nullptr)
+	{
+		return;
+	}
+		
+	aiString TextureFile;
+	const UINT TextureCount = Material->GetTextureCount(InTextureType);
+	TargetList->reserve(TextureCount);
+
+	for (UINT TextureIndex = 0; TextureIndex < TextureCount; TextureIndex++)
+	{
+		Material->GetTexture(InTextureType, TextureIndex, &TextureFile);
+		TargetList->emplace_back(TextureFile.C_Str());
+	}
+}
