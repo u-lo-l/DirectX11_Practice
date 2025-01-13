3d6719c42a0be1ccc67d77d4c20846e19aebfeb8
ULOL <jeffk0408@naver.com>
2025-01-13 09:38:22 +0900

    refactor : switch문 if문으로 변경

--- AssimpLoader/AssimpLoader/Types.cpp
+++ AssimpLoader/AssimpLoader/Types.cpp
@@ -1,27 +1,19 @@
 ﻿#include "Pch.h"
 #include "Types.h"
 
+// const aiMaterial * const : 함수 내에서 Material의 불변성을 보장
 void MaterialData::SetTextureFilesFromAiMaterialByTextureType( const aiMaterial * const Material, aiTextureType InTextureType )
 {
 	vector<string> * TargetList = nullptr;
-	switch (InTextureType)
-	{
-	case aiTextureType_DIFFUSE:  // NOLINT(bugprone-branch-clone)
-		TargetList = &DiffuseFiles;
-		break;
-	case aiTextureType_SPECULAR:
-		TargetList = &SpecularFiles;
-		break;
-	case aiTextureType_NORMALS:
-		TargetList = &NormalFiles;
-		break;
-	default:
-		break;
-	}
-	if (TargetList == nullptr)
-	{
-		return;
-	}
+
+	if (InTextureType==aiTextureType_DIFFUSE)
+		TargetList = &this->DiffuseFiles;
+	else if (InTextureType==aiTextureType_SPECULAR)
+		TargetList = &this->SpecularFiles;
+	else if (InTextureType==aiTextureType_NORMALS)
+		TargetList = &this->NormalFiles;
+	else
+		return ;
 		
 	aiString TextureFile;
 	const UINT TextureCount = Material->GetTextureCount(InTextureType);
